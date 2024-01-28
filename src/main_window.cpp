79ac3b87323959412047d8d7a0feaa8f27ffab9b
Pavel Ditenbir <pavel.ditenbir@gmail.com>
2024-01-28 12:07:51 +0700

    create app skeleton

---
+++ src/main_window.cpp
@@ -0,0 +1,15 @@
+#include "main_window.h"
+#include "src/ui/ui_main_window.h"
+
+Dialog::Dialog(QWidget *parent)
+    : QDialog(parent)
+    , ui(new Ui::Dialog)
+{
+    ui->setupUi(this);
+}
+
+Dialog::~Dialog()
+{
+    delete ui;
+}
+
